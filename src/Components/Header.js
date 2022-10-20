4142fb844b48b7a2ee01e404dcb75cf06785ae26
rakshit koyani <rakshitkoyani456@gmail.com>
2022-10-20 16:30:39 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ThemeToggler from "./ThemeToggler";
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Context API theme toggler</h2>
+      <ThemeToggler />
+    </header>
+  );
+}
+
+ export default Header;
